9cb2111189da1a81b6e3998c3b0477fe7e65370f
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-09-07 11:18:46 -0300

    update git

---
+++ MinerClicker_API/Models/Usuario.cs
@@ -0,0 +1,11 @@
+namespace MinerClicker_API.Models
+{
+public class Usuario
+        {
+        public int id { get; set; }
+        public string? nome { get; set; }
+        public string? email { get; set; }
+        public string? senha { get; set; }
+
+        }
+}
